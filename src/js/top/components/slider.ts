e6df5c413b086c5044baea2d90c119eeb0ecd44f
giselle <gisellesan0801@gmail.com>
2023-05-17 16:28:31 +0800

    changes in slider arrows and removed pagination

--- src/js/top/components/slider.ts
+++ src/js/top/components/slider.ts
@@ -1,6 +1,11 @@
 import Splide from '@splidejs/splide';
 const slider = () => {
-	new Splide('.splide').mount();
+	new Splide( '.splide', {
+		type: 'loop',
+		pagination: false,
+		speed: 700,
+	}
+	).mount();
 }
   
 export default slider
