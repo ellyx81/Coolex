4aac37dd42825e1b4c0a06645c4777b3348b172a
giselle <gisellesan0801@gmail.com>
2023-05-22 12:52:19 +0800

    disabled all links/button/vertical scrollbar when the modal is opened

--- src/js/top/components/video.ts
+++ src/js/top/components/video.ts
@@ -10,15 +10,18 @@ const videoPlayer = () => {
       const vidFrame = videoOverlay.querySelector('.video__frame') as HTMLElement;
       const originalSrc = vidFrame.getAttribute('src') as string;
       
-      video.addEventListener("click", () => {
+      video.addEventListener("click", () => { //open the video
         video.classList.add("is-opened");
-        html.style.overflowY = 'hidden';
+        html.style.overflowY = 'hidden'; //disable the vertical scrollbar when the video is opened
+        html.style.pointerEvents = 'none'; //disable all link/buttons outside the video modal
+        videoOverlay.style.pointerEvents = 'auto'; //excempt the video overlay from disabling
       })
 
-       close.addEventListener("click", () => {
+       close.addEventListener("click", () => { //close the video
          video.classList.remove("is-opened");
-         html.style.overflowY = 'visible';
+         html.style.overflowY = 'visible'; //undo the disabling vertical scrollbar
          vidFrame.setAttribute('src', originalSrc); //stop the video by refreshing the url
+         html.style.pointerEvents = 'auto'; //undo the disabling all link/buttons
        })
     })
   }
